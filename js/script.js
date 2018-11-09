2fd8de0be13cc9daca74aea2387e51cf04517209
Rachel <rachelkhoong@gmail.com>
2018-11-09 23:29:25 +1100

    Achieve Meets Expectations Requirements

--- js/script.js
+++ js/script.js
@@ -1,20 +1,109 @@
 let removealert = document.querySelector('main .alert .removealert');
-removealert.addEventListener('click', (e)=>{
+removealert.addEventListener('click', (e) => {
   let alert = e.target.parentNode.parentNode;
   let main = alert.parentNode;
   alert.style.opacity = '0';
-  setTimeout(function(){main.removeChild(alert);}, 301);
+  setTimeout(function() {
+    main.removeChild(alert);
+  }, 301);
 })
 
 let messagebutton = document.querySelector('#messagebutton');
-messagebutton.addEventListener('click', (e)=>{
+messagebutton.addEventListener('click', (e) => {
   let message = e.target.previousElementSibling;
   let user = e.target.previousElementSibling.previousElementSibling;
-  if (message.value=="" || user.value==""){
+  if (message.value == "" || user.value == "") {
     window.alert("You are missing a user or message");
   } else {
     window.alert("Message sent to " + user.value + "!");
     message.value = "";
     user.value = "";
+  }
+});
+
+let lineChartCont = document.getElementById("lineChart").getContext('2d');
+let lineChart = new Chart(lineChartCont, {
+  type: 'line',
+  data: {
+    labels: ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"],
+    datasets: [{
+      label: 'Visitors',
+      data: [1000, 870, 1204, 1300, 1260, 1584, 1496, 1349, 1792, 1572, 1504, 2059],
+      backgroundColor: 'rgba(69, 155, 128, 0.4)',
+      borderColor: '#459b80',
+      borderWidth: 2,
+    }]
+  },
+  options:{
+    aspectRatio:3,
+    legend:{
+      display:false,
+    },
+    elements:{
+      point:{
+        backgroundColor:'white',
+      },
+      line:{
+        tension:0,
+      }
+    }
+  }
+});
+
+
+let barChartCont = document.getElementById("barChart").getContext('2d');
+let barChart = new Chart(barChartCont, {
+  type: 'bar',
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+      label: 'Visitors',
+      data: [50, 40, 60, 24, 60, 64, 67],
+      backgroundColor: '#459b80',
+      borderColor: '#459b80',
+      borderWidth: 2,
+    }]
+  },
+  options:{
+    legend:{
+      display:false,
+    },
+    elements:{
+      point:{
+        backgroundColor:'white',
+      },
+      line:{
+        tension:0,
+      }
+    }
+  }
+});
+
+let mq = window.matchMedia("(min-width: 600px)");
+let legendPosition;
+if (mq.matches){
+  legendPosition = 'right';
+} else {
+  legendPosition = 'top';
+}
+
+let donutChartCont = document.getElementById("donutChart").getContext('2d');
+let donutChart = new Chart(donutChartCont, {
+  type: 'doughnut',
+  data: {
+    labels: ["Phones", "Tablets", "Desktops"],
+    datasets: [{
+      label: 'Visitors',
+      data: [70, 20, 10],
+      backgroundColor: ['#459b80', '#46B2AF', '#4290A8'],
+    }]
+  },
+  options:{
+    legend:{
+      position: legendPosition,
+      labels:{
+        boxWidth:15,
+      }
+    },
   }
 });
